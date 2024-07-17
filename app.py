e3bda6afade02d1e4595177c4c290baba0e3697e
Satyam Shukla <satyam.shukla2498@gmail.com>
2024-07-17 22:39:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,10 +26,10 @@ def recommend(movie):
     return recommended_movies,recommended_movies_poster
 
 
-movies_dict = pd.read_pickle('movies.pkl', 'rb')
+movies_dict = pd.read_pickle('movies.pkl', compression='infer')
 movies = pd.DataFrame(movies_dict)
 
-similarity = pd.read_pickle('similarity.pkl', 'rb')
+similarity = pd.read_pickle('similarity.pkl', compression='infer')
 
 st.title("Movie Recommender System")
 
